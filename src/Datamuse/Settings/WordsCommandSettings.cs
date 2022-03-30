5a1397c0048fd483c36db80b76319423be69ec89
Jack Savage <jacksav@protonmail.com>
2022-03-29 22:04:50 -0700

    First draft at word command settings

--- src/Datamuse/Settings/WordsCommandSettings.cs
+++ src/Datamuse/Settings/WordsCommandSettings.cs
@@ -1,7 +1,121 @@
+using System.ComponentModel;
+
+using Spectre.Console;
 using Spectre.Console.Cli;
 namespace Datamuse.Settings;
 
 class WordsSettings : CommandSettings
 {
+    // valid codes for the `Vocabulary` option
+    private static readonly HashSet<string> Vocabularies = new()
+    { 
+        "es",
+        "enwiki",
+    };
 
+    // valid codes for the `Related` option
+    private static readonly HashSet<string> RelationCodes = new()
+    {
+        "jja",
+        "jjb",
+        "syn",
+        "trg",
+        "ant",
+        "spc",
+        "gen",
+        "com",
+        "par",
+        "bga",
+        "bgb",
+        "rhy",
+        "nry",
+        "hom",
+        "cns",
+    };
+
+    // valid characters for the `MetaDataFlags` option
+    private static readonly HashSet<char> ValidMetadataFlags = new()
+    {
+        'd', // definitions
+        'p', // parts of speech
+        's', // syllable count
+        'r', // pronunciation
+        'f', // word frequency
+    };
+
+    [Description("Require that the results have a meaning related to this string value")]
+    [CommandOption("-m,--mean")]
+    public string? MeansLike { get; set; }
+
+    [Description("Require that the results are pronounced similarly to this string of characters")]
+    [CommandOption("-s,--sound")]
+    public string? SoundsLike { get; set; }
+
+    [Description("Require that the results are spelled similarly to this string of characters")]
+    [CommandOption("-p,--spell")]
+    public string? SpelledLike { get; set; }
+
+    [Description("Require that the results are in a predefined lexical relation to a word")]
+    [CommandOption("-r,--related")]
+    public (string Code, string Word)[]? Related { get; set; }
+
+    [Description("Identifier for the vocabulary to use")]
+    [CommandOption("-v,--vocab")]
+    public string? Vocabulary { get; set; }
+
+    [Description("Up to five hints about the theme of the document being written")]
+    [CommandOption("-t,--topics")]
+    public string[]? Topics { get; set; }
+
+    [Description("A hint about the word that appears immediately to the left of the target word in a sentence")]
+    [CommandOption("--left")]
+    public string? LeftContext { get; set; }
+
+    [Description("A hint about the word that appears immediately to the right of the target word in a sentence")]
+    [CommandOption("--right")]
+    public string? RightContext { get; set; }
+
+    [Description("Maximum number of results")]
+    [CommandOption("--max")]
+    [DefaultValue(100)]
+    public int? Maximum { get; set; }
+
+    [Description("Single-letter codes (no delimiter) requesting extra lexical knowledge")]
+    [CommandOption("--meta")]
+    public string? MetadataFlags { get; set; }
+
+    [Description("Prepend a result to the output that describes the query string from some other parameter")]
+    [CommandOption("-e,--query-echo")]
+    public string? QueryEcho { get; set; }
+
+    public override ValidationResult Validate()
+    {
+        static string list<T>(IEnumerable<T> items) =>
+            string.Join(", ", items);
+
+        // valid vocabulary
+        if (
+            Vocabulary is not null
+            && !Vocabularies.Contains(Vocabulary.ToLower())
+        ) return ValidationResult.Error($"Vocabulary must be one of {list(Vocabularies)}");
+
+        // valid relation code
+        if (
+            Related is not null
+            && Related.Any(r => !RelationCodes.Contains(r.Code))
+        ) return ValidationResult.Error($"Relation codes must be one of {list(RelationCodes)}");
+
+        // valid maximum
+        const int maxMax = 1000;
+        if (Maximum is not null && (Maximum < 0 || Maximum > maxMax))
+            return ValidationResult.Error($"Maximum must be non-negative and less than {maxMax}");
+
+        // valid metadata flags
+        if (
+            MetadataFlags is not null
+            && MetadataFlags.Any(f => !ValidMetadataFlags.Contains(f))
+        ) return ValidationResult.Error($"Metadata flags must be one of {list(ValidMetadataFlags)}")
+
+        return ValidationResult.Success();
+    }
 }
