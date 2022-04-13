7abb2e6408825c81808134e4f2547deeaeb7e4a4
Jack Savage <jacksav@protonmail.com>
2022-04-13 11:15:12 -0700

    Implement settings for SuggestionCommand

--- src/Datamuse/Settings/SuggestCommandSettings.cs
+++ src/Datamuse/Settings/SuggestCommandSettings.cs
@@ -1,7 +1,27 @@
+using System.ComponentModel;
+using Spectre.Console;
 using Spectre.Console.Cli;
+
 namespace Datamuse.Settings;
 
 class SuggestCommandSettings : CommandSettings
 {
+    [Description("Prefix hint string; typically, the characters that the user has entered so far into a search box")]
+    [CommandArgument(0, "<hint-string>")]
+    public string? HintString { get; set; }
 
+    [Description("Maximum number of results")]
+    [CommandOption("--max")]
+    [DefaultValue(10)]
+    public int? Maximum { get; set; }
+
+    [Description("Identifier for the vocabulary to use")]
+    [CommandOption("-v,--vocab")]
+    public string? Vocabulary { get; set; }
+
+    public override ValidationResult Validate() =>
+        Validation.ValidateUntilError(new[] {
+            () => Validation.ValidateMaximum(Maximum),
+            () => Validation.ValidateVocabulary(Vocabulary),
+        });
 }
