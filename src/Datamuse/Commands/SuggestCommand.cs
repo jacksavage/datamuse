fc93ecd053ef17721adbeab3af619f938e33c4bb
Jack Savage <jacksav@protonmail.com>
2022-04-13 11:23:48 -0700

    Implement debug view Prints all args when you call command

--- src/Datamuse/Commands/SuggestCommand.cs
+++ src/Datamuse/Commands/SuggestCommand.cs
@@ -1,6 +1,7 @@
 using Spectre.Console.Cli;
 using Datamuse.Settings;
 using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
 
 namespace Datamuse.Commands;
 
@@ -8,6 +9,10 @@ class SuggestCommand : Command<SuggestCommandSettings>
 {
     public override int Execute([NotNull] CommandContext context, [NotNull] SuggestCommandSettings settings)
     {
-        throw new NotImplementedException();
+        AnsiConsole.WriteLine($"HintString : {settings.HintString}");
+        AnsiConsole.WriteLine($"Maximum : {settings.Maximum}");
+        AnsiConsole.WriteLine($"Vocabulary : {settings.Vocabulary}");
+
+        return 0;
     }
 }
--- src/Datamuse/Commands/WordsCommand.cs
+++ src/Datamuse/Commands/WordsCommand.cs
@@ -1,6 +1,7 @@
 using Spectre.Console.Cli;
 using Datamuse.Settings;
 using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
 
 namespace Datamuse.Commands;
 
@@ -8,6 +9,19 @@ class WordsCommand : Command<WordsSettings>
 {
     public override int Execute([NotNull] CommandContext context, [NotNull] WordsSettings settings)
     {
-        throw new NotImplementedException();
+        AnsiConsole.WriteLine($"MeansLike : {settings.MeansLike}");
+        AnsiConsole.WriteLine($"SoundsLike : {settings.SoundsLike}");
+        AnsiConsole.WriteLine($"SpelledLike : {settings.SpelledLike}");
+        AnsiConsole.WriteLine($"Related : {settings.Related}");
+        AnsiConsole.WriteLine($"Vocabulary : {settings.Vocabulary}");
+        AnsiConsole.WriteLine($"Topics : {settings.Topics}");
+        AnsiConsole.WriteLine($"LeftContext : {settings.LeftContext}");
+        AnsiConsole.WriteLine($"RightContext : {settings.RightContext}");
+        AnsiConsole.WriteLine($"Maximum : {settings.Maximum}");
+        AnsiConsole.WriteLine($"MetadataFlags : {settings.MetadataFlags}");
+        AnsiConsole.WriteLine($"QueryEcho : {settings.QueryEcho}");
+        AnsiConsole.WriteLine($"Validate : {settings.Validate}");
+
+        return 0;
     }
 }
