a58f76faf81fcbf9989bcf38dd41f2cebdd4f532
Jack Savage <jacksav@protonmail.com>
2022-04-13 16:55:51 -0700

    Use ApiService Dummy endpoints for now

--- src/Datamuse/Commands/SuggestCommand.cs
+++ src/Datamuse/Commands/SuggestCommand.cs
@@ -2,16 +2,22 @@ using Spectre.Console.Cli;
 using Datamuse.Settings;
 using System.Diagnostics.CodeAnalysis;
 using Spectre.Console;
+using Datamuse.Services;
 
 namespace Datamuse.Commands;
 
 class SuggestCommand : Command<SuggestCommandSettings>
 {
+    private readonly IApiService _apiService;
+
+    public SuggestCommand(IApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
     public override int Execute([NotNull] CommandContext context, [NotNull] SuggestCommandSettings settings)
     {
-        AnsiConsole.WriteLine($"Hint : {settings.Hint}");
-        AnsiConsole.WriteLine($"Maximum : {settings.Maximum}");
-        AnsiConsole.WriteLine($"Vocabulary : {settings.Vocabulary}");
+        AnsiConsole.WriteLine(_apiService.GetSuggestions());
 
         return 0;
     }
--- src/Datamuse/Commands/WordsCommand.cs
+++ src/Datamuse/Commands/WordsCommand.cs
@@ -2,25 +2,22 @@ using Spectre.Console.Cli;
 using Datamuse.Settings;
 using System.Diagnostics.CodeAnalysis;
 using Spectre.Console;
+using Datamuse.Services;
 
 namespace Datamuse.Commands;
 
 class WordsCommand : Command<WordsSettings>
 {
+    private readonly IApiService _apiService;
+
+    public WordsCommand(IApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
     public override int Execute([NotNull] CommandContext context, [NotNull] WordsSettings settings)
     {
-        AnsiConsole.WriteLine($"MeansLike : {settings.MeansLike}");
-        AnsiConsole.WriteLine($"SoundsLike : {settings.SoundsLike}");
-        AnsiConsole.WriteLine($"SpelledLike : {settings.SpelledLike}");
-        AnsiConsole.WriteLine($"Related : {settings.Related}");
-        AnsiConsole.WriteLine($"Vocabulary : {settings.Vocabulary}");
-        AnsiConsole.WriteLine($"Topics : {settings.Topics}");
-        AnsiConsole.WriteLine($"LeftContext : {settings.LeftContext}");
-        AnsiConsole.WriteLine($"RightContext : {settings.RightContext}");
-        AnsiConsole.WriteLine($"Maximum : {settings.Maximum}");
-        AnsiConsole.WriteLine($"MetadataFlags : {settings.MetadataFlags}");
-        AnsiConsole.WriteLine($"QueryEcho : {settings.QueryEcho}");
-        AnsiConsole.WriteLine($"PhoneticAlphabet : {settings.PhoneticAlphabet}");
+        AnsiConsole.WriteLine(_apiService.GetWords());
 
         return 0;
     }
