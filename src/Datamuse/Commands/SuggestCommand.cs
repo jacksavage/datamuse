78c1a24a1861e6fe1e75799e7a9096c41ce509ca
Jack Savage <jacksav@protonmail.com>
2022-04-17 15:15:54 -0700

    Add params to suggest and use new Result[] response

--- src/Datamuse/Commands/SuggestCommand.cs
+++ src/Datamuse/Commands/SuggestCommand.cs
@@ -3,6 +3,7 @@ using Datamuse.Settings;
 using System.Diagnostics.CodeAnalysis;
 using Spectre.Console;
 using Datamuse.Services;
+using Datamuse.Models;
 
 namespace Datamuse.Commands;
 
@@ -18,17 +19,25 @@ class SuggestCommand : Command<SuggestCommandSettings>
     public override int Execute([NotNull] CommandContext context, [NotNull] SuggestCommandSettings settings)
     {
         // map settings to request parameter keys
-        Dictionary<string, string> parameters = new()
+        Dictionary<string, string> parameters = new();
+        void add<T>(string key, T value)
         {
+            if (value is not null)
+            {
+                parameters.Add(key, value.ToString());
+            }
+        }
+        add("s", settings.Hint);
+        add("v", settings.Vocabulary);
+        add("max", settings.Maximum);
 
-        };
-
-        // get the suggestions and check for success
-        string? response = _apiService.GetSuggestions(parameters);
+        // get the words and check for success
+        Result[]? response = _apiService.GetSuggestions(parameters);
         if (response is null) return 1;
 
         // print the response to the user
-        AnsiConsole.WriteLine(response);
+        string print = string.Join("\n", response.Select(r => r.Word ?? ""));
+        AnsiConsole.WriteLine(print);
         return 0;
     }
 }
